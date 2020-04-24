dad99e8a0fc39fbbaccf27433d9594d60d8f54dd
lisz1012 <lisz1012@163.com>
2020-04-24 01:49:43 -0700

    Unit test sometimes can't consume the messages, now use the Applicationn class to send the messages

--- src/main/java/com/lisz/SpringbootKafkaApplication.java
+++ src/main/java/com/lisz/SpringbootKafkaApplication.java
@@ -1,8 +1,14 @@
 package com.lisz;
 
+import com.lisz.service.IMessageSender;
+import com.lisz.service.MessageSender;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.KafkaListeners;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -11,8 +17,16 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class SpringbootKafkaApplication {
-    public static void main( String[] args ) throws IOException {
-        SpringApplication.run(SpringbootKafkaApplication.class, args);
+
+    public static void main( String[] args ) throws Exception {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootKafkaApplication.class, args);
+        Thread.sleep(5000);
+        IMessageSender sender = context.getBean(IMessageSender.class);
+        for (int i = 0; i < 9; i++) {
+            sender.send("topic01", "lisz - " + i, "lisz1012 - " + i);
+            Thread.sleep(1000);
+        }
+
         System.in.read();
     }
 
@@ -25,5 +39,9 @@ public class SpringbootKafkaApplication {
     @SendTo("topic03")  // 将String返回值转发给topic03
     public String receive02(ConsumerRecord<String, String> record) {
         return record.value() + " --- lisz";
+    }
+
+    public void send() {
+
     }
 }
